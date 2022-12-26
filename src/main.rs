6aa7fcee2d0fec15a13649c82df6ecb00bd1d09d
Michael Zill <michael.zill@gmail.com>
2022-12-26 11:00:35 +0100

    initial

---
+++ src/main.rs
@@ -0,0 +1,174 @@
+#![feature(type_alias_impl_trait)]
+
+use edge_executor::{Local, Task};
+use embassy_futures::select::{select, Either};
+use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
+use esp_idf_sys::EspError;
+
+use crate::errors::*;
+use channel_bridge::{asynch::pubsub, asynch::*, notification::Notification};
+use edge_executor::*;
+use embedded_svc::utils::asyncify::Asyncify;
+use embedded_svc::wifi::{AuthMethod, ClientConfiguration, Configuration, Wifi as WifiTrait};
+use esp_idf_hal::modem::WifiModemPeripheral;
+use esp_idf_hal::peripheral::Peripheral;
+use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_hal::task::executor::EspExecutor;
+use esp_idf_hal::task::thread::ThreadSpawnConfiguration;
+use esp_idf_svc::eventloop::EspSystemEventLoop;
+use esp_idf_svc::netif::IpEvent;
+use esp_idf_svc::nvs::EspDefaultNvsPartition;
+use esp_idf_svc::wifi::{EspWifi, WifiEvent};
+use esp_idf_sys::{self as sys};
+use log::*;
+
+mod errors;
+
+sys::esp_app_desc!();
+
+const SSID: &str = env!("RUST_ESP32_ANEMOMETER_WIFI_SSID");
+const PASS: &str = env!("RUST_ESP32_ANEMOMETER_WIFI_PASS");
+
+const TASK_ID_PRIORITY: u8 = 30;
+
+fn main() -> anyhow::Result<()> {
+    esp_idf_hal::task::critical_section::link();
+    esp_idf_svc::timer::embassy_time::driver::link();
+    esp_idf_svc::timer::embassy_time::queue::link();
+    // Temporary. Will disappear once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
+    // or else some patches to the runtime implemented by esp-idf-sys might not link properly.
+    //esp_idf_sys::link_patches();
+
+    esp_idf_svc::log::EspLogger::initialize_default();
+    info!("Hello, world!");
+
+    info!("Wifi name {}", SSID);
+
+    let peripherals = Peripherals::take().unwrap();
+    let nvs_default_partition = EspDefaultNvsPartition::take()?;
+    let sysloop = EspSystemEventLoop::take()?;
+
+    let (wifi, wifi_notif) = wifi(
+        peripherals.modem,
+        sysloop.clone(),
+        Some(nvs_default_partition.clone()),
+    )?;
+
+    ThreadSpawnConfiguration {
+        name: Some(b"async-exec-mid\0"),
+        priority: TASK_ID_PRIORITY,
+        ..Default::default()
+    }
+    .set()?;
+
+    let mid_prio_execution = schedule::<8, _>(50000, move || {
+        let executor = EspExecutor::new();
+        let mut tasks = heapless::Vec::new();
+
+        executor.spawn_local_collect(process_wifi_state_change(wifi, wifi_notif), &mut tasks)?;
+
+        let netif_notif = netif_notifier(sysloop.clone()).unwrap(); // TODO: error conversion
+        executor.spawn_local_collect(process_netif_state_change(netif_notif), &mut tasks)?;
+
+        Ok((executor, tasks))
+    });
+
+    mid_prio_execution.join().unwrap();
+
+    Ok(())
+}
+
+pub fn schedule<'a, const C: usize, M>(
+    stack_size: usize,
+    spawner: impl FnOnce() -> Result<(Executor<'a, C, M, Local>, heapless::Vec<Task<()>, C>), SpawnError>
+        + Send
+        + 'static,
+) -> std::thread::JoinHandle<()>
+where
+    M: Monitor + Wait + Default,
+{
+    std::thread::Builder::new()
+        .stack_size(stack_size)
+        .spawn(move || {
+            let (executor, tasks) = spawner().unwrap();
+
+            executor.run_tasks(|| true, tasks);
+        })
+        .unwrap()
+}
+
+#[inline(always)]
+pub fn netif_notifier(
+    mut sysloop: EspSystemEventLoop,
+) -> Result<impl Receiver<Data = IpEvent>, InitError> {
+    Ok(pubsub::SvcReceiver::new(sysloop.as_async().subscribe()?))
+}
+
+pub fn wifi<'d>(
+    modem: impl Peripheral<P = impl WifiModemPeripheral + 'd> + 'd,
+    mut sysloop: EspSystemEventLoop,
+    partition: Option<EspDefaultNvsPartition>,
+) -> Result<(impl WifiTrait + 'd, impl Receiver<Data = WifiEvent>), EspError> {
+    let mut wifi = EspWifi::new(modem, sysloop.clone(), partition)?;
+
+    if PASS.is_empty() {
+        wifi.set_configuration(&Configuration::Client(ClientConfiguration {
+            ssid: SSID.into(),
+            auth_method: AuthMethod::None,
+            ..Default::default()
+        }))?;
+    } else {
+        wifi.set_configuration(&Configuration::Client(ClientConfiguration {
+            ssid: SSID.into(),
+            password: PASS.into(),
+            ..Default::default()
+        }))?;
+    }
+
+    wifi.start()?;
+
+    wifi.connect()?;
+
+    Ok((
+        wifi,
+        pubsub::SvcReceiver::new(sysloop.as_async().subscribe()?),
+    ))
+}
+
+pub async fn process_wifi_state_change(
+    mut wifi: impl WifiTrait,
+    mut state_changed_source: impl Receiver<Data = WifiEvent>,
+) {
+    loop {
+        let event = state_changed_source.recv().await;
+        let event = event.unwrap();
+        match event {
+            WifiEvent::StaConnected => {
+                info!("WifiEvent: STAConnected");
+            }
+            WifiEvent::StaDisconnected => {
+                info!("WifiEvent: STADisconnected");
+                wifi.connect();
+            }
+            _ => {
+                info!("WifiEvent: other .....");
+            }
+        }
+    }
+}
+
+pub async fn process_netif_state_change(mut state_changed_source: impl Receiver<Data = IpEvent>) {
+    loop {
+        let event = state_changed_source.recv().await;
+        let event = event.unwrap();
+
+        match event {
+            IpEvent::DhcpIpAssigned(assignment) => {
+                info!("IpEvent: DhcpIpAssigned: {:?}", assignment.ip_settings.ip);
+            }
+            _ => {
+                info!("IpEvent: other .....");
+            }
+        }
+    }
+}
