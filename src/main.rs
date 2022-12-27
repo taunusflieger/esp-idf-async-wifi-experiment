eb95877ee7e8ed6360652e64b87fb5489b29a2fd
Michael Zill <michael.zill@gmail.com>
2022-12-27 17:03:40 +0100

    auto start/stop httpd depending on network state

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 #![feature(type_alias_impl_trait)]
 
-use edge_executor::{Local, Task};
-
-use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
-use esp_idf_sys::EspError;
-
 use crate::errors::*;
+use crate::lazy_http_server::lazy_init_http_server::LazyInitHttpServer;
 use channel_bridge::{asynch::pubsub, asynch::*};
 use edge_executor::*;
+use edge_executor::{Local, Task};
+use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
+use embassy_sync::pubsub::PubSubChannel;
+use embassy_time::{Duration, Timer};
 use embedded_svc::utils::asyncify::Asyncify;
 use embedded_svc::wifi::{AuthMethod, ClientConfiguration, Configuration, Wifi as WifiTrait};
 use esp_idf_hal::modem::WifiModemPeripheral;
@@ -19,10 +19,13 @@ use esp_idf_svc::eventloop::EspSystemEventLoop;
 use esp_idf_svc::netif::IpEvent;
 use esp_idf_svc::nvs::EspDefaultNvsPartition;
 use esp_idf_svc::wifi::{EspWifi, WifiEvent};
+use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
+use esp_idf_sys::EspError;
 use esp_idf_sys::{self as sys};
 use log::*;
 
 mod errors;
+mod lazy_http_server;
 
 sys::esp_app_desc!();
 
@@ -30,6 +33,31 @@ const SSID: &str = env!("RUST_ESP32_ANEMOMETER_WIFI_SSID");
 const PASS: &str = env!("RUST_ESP32_ANEMOMETER_WIFI_PASS");
 
 const TASK_ID_PRIORITY: u8 = 30;
+static CHANNEL: PubSubChannel<CriticalSectionRawMutex, [u8; 8], 4, 4, 4> = PubSubChannel::new();
+static NETWORK_EVENT_CHANNEL: PubSubChannel<CriticalSectionRawMutex, NetworkStateChange, 4, 4, 4> =
+    PubSubChannel::new();
+
+#[allow(dead_code)]
+static APPLICATION_EVENT_CHANNEL: PubSubChannel<
+    CriticalSectionRawMutex,
+    ApplicationStateChange,
+    4,
+    4,
+    4,
+> = PubSubChannel::new();
+
+#[derive(Copy, Clone, Debug)]
+enum NetworkStateChange {
+    WifiDisconnected,
+    IpAddressAssigned { ip: embedded_svc::ipv4::Ipv4Addr },
+}
+
+#[derive(Copy, Clone, Debug)]
+#[allow(dead_code)]
+enum ApplicationStateChange {
+    OTAUpdateRequest,
+    OTAUpdateStarted,
+}
 
 fn main() -> anyhow::Result<()> {
     esp_idf_hal::task::critical_section::link();
@@ -64,6 +92,14 @@ fn main() -> anyhow::Result<()> {
 
         executor.spawn_local_collect(process_wifi_state_change(wifi, wifi_notif), &mut tasks)?;
 
+        executor.spawn_local_collect(channel_publisher_task(), &mut tasks)?;
+
+        executor.spawn_local_collect(channel_subscriber1_task(), &mut tasks)?;
+
+        executor.spawn_local_collect(channel_subscriber2_task(), &mut tasks)?;
+
+        executor.spawn_local_collect(http_server_task(), &mut tasks)?;
+
         let netif_notif = netif_notifier(sysloop.clone()).unwrap(); // TODO: error conversion
         executor.spawn_local_collect(process_netif_state_change(netif_notif), &mut tasks)?;
 
@@ -72,7 +108,7 @@ fn main() -> anyhow::Result<()> {
 
     mid_prio_execution.join().unwrap();
 
-    Ok(())
+    unreachable!();
 }
 
 pub fn schedule<'a, const C: usize, M>(
@@ -145,6 +181,8 @@ pub async fn process_wifi_state_change(
             }
             WifiEvent::StaDisconnected => {
                 info!("WifiEvent: STADisconnected");
+                let mut publisher = NETWORK_EVENT_CHANNEL.publisher().unwrap();
+                let _ = publisher.send(NetworkStateChange::WifiDisconnected).await;
                 let _ = wifi.connect();
             }
             _ => {
@@ -161,9 +199,85 @@ pub async fn process_netif_state_change(mut state_changed_source: impl Receiver<
         match event {
             IpEvent::DhcpIpAssigned(assignment) => {
                 info!("IpEvent: DhcpIpAssigned: {:?}", assignment.ip_settings.ip);
+                let mut publisher = NETWORK_EVENT_CHANNEL.publisher().unwrap();
+                let _ = publisher
+                    .send(NetworkStateChange::IpAddressAssigned {
+                        ip: assignment.ip_settings.ip,
+                    })
+                    .await;
             }
             _ => {
                 info!("IpEvent: other .....");
+            }
+        }
+    }
+}
+
+async fn channel_publisher_task() {
+    let mut buf = [0; 8];
+    buf.copy_from_slice(b"Hello!\r\n");
+    loop {
+        let mut publisher = CHANNEL.publisher().unwrap();
+        let _ = publisher.send(buf).await;
+        info!("send: {:?}", buf);
+        Timer::after(Duration::from_secs(10)).await;
+    }
+}
+
+async fn channel_subscriber1_task() {
+    loop {
+        let mut subscriber = CHANNEL.subscriber().unwrap();
+        let buf = subscriber.recv().await;
+        info!("Sub 1 received: {:?}", buf);
+    }
+}
+
+async fn channel_subscriber2_task() {
+    loop {
+        let mut subscriber = CHANNEL.subscriber().unwrap();
+        let buf = subscriber.recv().await;
+        info!("Sub 2 received: {:?}", buf);
+    }
+}
+
+async fn http_server_task() {
+    use embedded_svc::{io::Write, utils::http::Headers};
+    use esp_idf_svc::http::server::Configuration;
+
+    const FIRMWARE_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+    let httpd = LazyInitHttpServer::new();
+    loop {
+        let mut subscriber = NETWORK_EVENT_CHANNEL.subscriber().unwrap();
+        let event = subscriber.recv().await.unwrap();
+
+        match event {
+            NetworkStateChange::IpAddressAssigned { ip } => {
+                let conf = Configuration::default();
+                let mut s = httpd.create(&conf);
+
+                info!("http_server_task: starting httpd on address: {:?}", ip);
+                if let Err(err) = s.fn_handler("/", embedded_svc::http::Method::Get, move |req| {
+                    let mut headers = Headers::<1>::new();
+                    headers.set_cache_control("no-store");
+
+                    let mut resp = req.into_response(200, None, headers.as_slice())?;
+                    resp.write_all(FIRMWARE_VERSION.as_bytes())?;
+                    info!("Processing '/api/version' request");
+                    Ok(())
+                }) {
+                    info!(
+                        "http_server_task: failed to register http handler /: {:?} - restarting device",
+                        err
+                    );
+                    unsafe {
+                        esp_idf_sys::esp_restart();
+                    }
+                }
+            }
+            NetworkStateChange::WifiDisconnected => {
+                info!("http_server_task: stopping httpd");
+                httpd.clear();
             }
         }
     }
